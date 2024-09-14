fa9ae5d1fce580ee56ebcb85f0716f7083497cdf
Rishurp <rishupandey3102@gmail.com>
2024-09-14 22:11:42 +0530

    initialize routes for project

---
+++ app/(dashboard)/todos/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+console.log(process.argv);
+const setting = () => {
+  return <div>todos</div>;
+};
+
+export default setting;
---
+++ app/docs/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <div>doc</div>;
+};
+
+export default page;
